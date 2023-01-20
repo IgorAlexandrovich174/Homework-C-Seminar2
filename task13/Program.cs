a0034dd526e344c15a71bea492f515a78ffed57d
Igor Alexandrovich Ivanov <igoralexandrovich95@mail.ru>
2023-01-20 13:28:05 +0500

    Completed task13

--- task13/Program.cs
+++ task13/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//
+//645 -> 5
+//
+//78 -> третьей цифры нет
+//
+//32679 -> 6
+
+System.Console.Write("Введите число: ");
+
+string inputValue = System.Console.ReadLine();
+
+if (inputValue.Length < 3 ) {
+    System.Console.WriteLine("Третья цифра отсутствует!");
+} else {
+    System.Console.WriteLine(inputValue[2]);
+}
